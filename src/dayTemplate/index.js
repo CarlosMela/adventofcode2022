3cf4f0412ed610acb723e09bae0c3dd0bfb2bcf5
Carlos Moreno <carlos.moreno-rios@lottoland.com>
2022-12-13 07:23:56 +0100

    update template to use example input first

--- src/dayTemplate/index.js
+++ src/dayTemplate/index.js
@@ -1,8 +1,8 @@
 import '../util/lib.js'
-import {fetchInput} from '../util/onlineInput.js'
+import {readFile} from '../util/fileHelper.js'
 
-// const data = await readFile(new URL('./input.txt', import.meta.url))
-const data = await fetchInput(5)
+// const data = await fetchInput(5)
+const data = await readFile(new URL('./input.txt', import.meta.url))
 
 const part1 = data
     .split('\n')
